64f0fa3d29162bc464a604805fa9bb850bde6847
ELIZABETH SPENCER <erss@bu.edu>
2018-07-17 17:53:44 -0400

    fixed bugs

--- gen_surrogate_distr.m
+++ gen_surrogate_distr.m
@@ -2,7 +2,7 @@ function [ model ] = gen_surrogate_distr( model)
 % Shuffles channels in time + space to generate distribution of max abs
 % cross correlation values.
 
-data = model.data;
+data = model.data_clean;
 window_size = model.window_size * model.sampling_frequency;   % window size (samples)
 n = size(data,1);   % size of network
 T = size(data,2);   % length of network
@@ -10,14 +10,17 @@ nsurrogates = model.nsurrogates;
 
 mx = zeros(1,nsurrogates);
 lag = zeros(1,nsurrogates);
+taxis = model.dynamic_network_taxis;
 
 for ii = 1:nsurrogates
     
     ij = randperm(n,2); % Choose two random electrodes i & j, without replacement.
     i=ij(1);
     j=ij(2);
-    ti = randi(T-window_size+1); % Choose random time, i.
-    tj = randi(T-window_size+1); % Choose random time, j.
+    %ti = randi(T-window_size+1); % Choose random time, i.
+   % tj = randi(T-window_size+1); % Choose random time, j.
+    ti = randi(length(taxis));
+    tj = randi(length(taxis));
     
     xi = data(i,ti:(ti+window_size-1));
     xj = data(j,tj:(tj+window_size-1));
@@ -27,7 +30,7 @@ for ii = 1:nsurrogates
     
     mx(ii)  = mxij(3);
     lag(ii) = lagij(3);
-    
+    fprintf(num2st(ii),'\n')
 end
 
 % Store surrogate distribution, and lags where abs maximum values occur.
--- remove_artifacts.m
+++ remove_artifacts.m
@@ -18,12 +18,13 @@ RN  = find(contains(RDL,'temporal')) + 162;
 
 
 %%% Find time chunks with slope > - 1.5
-data = model.data([LN, RN],:);
+f0=model.sampling_frequency;
+data = model.data;
 m = mean(data,2);
 m = repmat(m,[1 size(data,2)]);
 data = data - m;
 
-d = data';
+d = data([LN, RN],:)';
 
 t = model.t;
 window_step = 0.5;
@@ -37,7 +38,7 @@ for k = 1:i_total
     t_stop  = t_start + window_size;                  %... get window stop time [s],
     indices = t >= t_start & t < t_stop;
     
-    subplot(2,1,1)
+ %   subplot(2,1,1)
     [Sxx, faxis] = pmtm(d(indices,:),4,sum(indices),f0);
     
     f_start = 30;
@@ -47,15 +48,15 @@ for k = 1:i_total
     X =log(faxis(f_indices));
     y = mean(log(Sxx(f_indices,:)),2);
     b = glmfit(X,y);
-    plot(X,b'*[ones(1,size(X,1));X'],'--r','LineWidth',2)
+ %   plot(X,b'*[ones(1,size(X,1));X'],'--r','LineWidth',2)
     slope(1,k)=t_start;
     slope(2,k) = t_stop;
     slope(3,k)=b(2);
-    
+    fprintf([num2str(k),'\n'])
 end
 
 
-indices = find(slope(3,:) <= -1.5);
+indices = find(slope(3,:) > -1.5);
 bad_channels = slope(:,indices);
 
 data_clean = data;
@@ -63,7 +64,7 @@ t_clean = t;
 for k = size(bad_channels,2):-1:1
     t_start = bad_channels(1,k);
     t_stop = bad_channels(2,k);
-    indx = t >= t_start & t < t_stop;
+    indx = find(t >= t_start & t <= t_stop);
     
     data_clean(:,indx)= [];
     t_clean(indx) =[];
